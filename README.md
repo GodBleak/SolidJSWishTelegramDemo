4366402974423ad2fa4d9edec19fb2040a5ee38d
GodBleak <38085518+GodBleak@users.noreply.github.com>
2022-05-10 18:53:03 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# SolidJSWishTelegramDemo
-Created with CodeSandbox
+# A demo of GramJS entirely in the browser with SolidJS.
+
+Uses the [SolidJS](https://github.com/solidjs/solid) [vite typescript starter template](https://github.com/solidjs/templates/tree/master/ts), with [Animate.css](https://github.com/animate-css/animate.css), [TailwindCSS](https://github.com/tailwindlabs/tailwindcss), and [DaisyUI](https://github.com/saadeghi/daisyui)
+
+
+## Usage
+
+Those templates dependencies are maintained via [pnpm](https://pnpm.io) via `pnpm up -Lri`.
+
+This is the reason you see a `pnpm-lock.yaml`. That being said, any package manager will work. This file can be safely be removed once you clone a template.
+
+```bash
+$ npm install # or pnpm install or yarn install
+```
+
+**You must also configure SSL**
+For dev purposes, I recommend using [mkcert](https://github.com/FiloSottile/mkcert#example) and passing your key and cert files to vite's [`server.https`](https://vitejs.dev/config/#server-https). Detailed instructions [here](https://stackoverflow.com/a/69743888). mkcert does have an [npm package](https://www.npmjs.com/package/mkcert), but I had no luck with that route, albeit I didn't try very hard.
+
+
+### Learn more on SolidJS at the [Solid Website](https://solidjs.com) and come chat with us on our [Discord](https://discord.com/invite/solidjs)
+### Learn more about GramJS at [their repo](https://github.com/gram-js/gramjs) and come chat with us on [Telegram](https://t.me/gramjschat)
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm dev` or `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br>
+
+### `npm run build`
+
+Builds the app for production to the `dist` folder.<br>
+It correctly bundles Solid in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+Your app is ready to be deployed!
+
+## Deployment
+
+You can deploy the `dist` folder to any static host provider (netlify, surge, now, etc.)
---
+++ src/login.tsx
@@ -0,0 +1,119 @@
+import { createEffect, createMemo, Show } from "solid-js"
+import type { Accessor, Component, Resource, Setter } from "solid-js";
+import { Api, TelegramClient } from 'telegram'
+
+
+// element refs -- for easy updating of the form elements
+let input: HTMLInputElement;
+let prompt: HTMLParagraphElement
+let button: HTMLButtonElement;
+let tooltip: HTMLDivElement;
+
+// props required for this component
+interface LoginProps {
+  loggedIn: Accessor<boolean>;
+  setLoggedIn: Setter<boolean>;
+  setLogInProptIsOpen: Setter<boolean>;
+  client: Resource<TelegramClient | null>
+}
+
+export const LogIn: Component<LoginProps> = (props) => {
+  // assign passed client instance to a local variable
+  const client = createMemo(() => props.client())  
+
+  // kickoff the logic of the login flow
+  createEffect(async (prev) => {
+    if ((prev !== props.loggedIn()) && client()) {
+        if (!props.loggedIn()) {
+            await initializeAuth(client()!);
+            const isAuthorized = await client()!.isUserAuthorized()
+            if(isAuthorized){
+              props.setLogInProptIsOpen(false)
+              localStorage.setItem("sessionString", client()!.session.save() as any as string)
+            }
+            props.setLoggedIn(isAuthorized)
+        }
+    }
+    return props.loggedIn()
+  })
+
+ async function logOut(){ // the logout function
+    client()?.invoke(new Api.auth.LogOut())
+    localStorage.removeItem("sessionString")
+    props.setLogInProptIsOpen(false)
+    props.setLoggedIn(false)
+  }
+
+  //defining the login modal
+  return (
+    <div class="modal modal-open">
+      <div class="modal-box flex flex-col w-min relative">
+        <button onclick={() => props.setLogInProptIsOpen(false)} class="btn btn-circle btn-xs absolute top-2 right-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+        </button>
+        <Show when={props.loggedIn()} fallback={
+          <>
+            <h3 class="font-bold text-lg uppercase text-center">Sign into Telegram</h3>
+            <span>
+                <p ref={prompt} class="py-4"></p> <div ref={tooltip} data-tip="Ensure this is an international format"><input ref={input} class="input input-primary" /></div>
+            </span>
+            <div class="modal-action">
+                <button ref={button} class="btn w-full">Next</button>
+            </div>
+          </>
+        }>
+          <h3 class="font-bold text-lg uppercase text-center pt-1">Goodbye!</h3>
+          <div class="modal-action">
+                <button onClick={async () => await logOut()} class="btn w-full btn-success">logout</button>
+            </div>
+        </Show>
+      </div>
+    </div>
+  );
+};
+
+// the function to that modifies the modal for the current auth state
+function askFor(request: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+      switch (request) {
+        case 'phoneNumber':
+            prompt.innerHTML = "Enter your phone number";
+            input.type = "tel";
+            input.placeholder = '12225551234' // give the user a hint as to the format we're expecting
+            button.onclick = () => (button.classList.toggle("loading"), resolve(input.value));
+            break;
+
+        case "phoneCode":
+            button.classList.toggle("loading")
+            input.value = ''; // clear the input for the user
+            input.type = "number" // help the user avoid typos
+            input.placeholder = "Enter your verification code";
+            prompt.innerText = "A verification was sent via Telegram or SMS.";
+            button.onclick = () => (button.classList.toggle("loading"), resolve(input.value));
+            break;
+
+        case "password":
+            button.classList.toggle("loading")
+            input.value = '';
+            input.type = "password";
+            input.placeholder = "Enter your password";
+            prompt.innerText = "Enter your password";
+            button.onclick = () => (button.classList.toggle("loading"), resolve(input.value));
+            break;
+
+        // shouldn't need a default case 
+      }
+  })
+}
+
+// the function that prompts the user for their credentials and signs them in 
+async function initializeAuth(client: TelegramClient) {
+  await client.start({
+      phoneNumber: async () => await askFor("phoneNumber"),
+      phoneCode: async () => await askFor("phoneCode"),
+      password: async () => await askFor("password"),
+      // TODO emplement actual error handling 
+      onError: console.error
+  })
+}
+
